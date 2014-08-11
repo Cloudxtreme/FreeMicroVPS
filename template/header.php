78b92680fbe0bc9b76e48922da8301baabb13818
br1ckd <bricked@null.net>
2014-08-10 17:47:29 -1000

    misc

--- template/header.php
+++ template/header.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
         <!--<link rel="stylesheet" href="/style.css">
         <link rel="stylesheet" href="/custom.css">-->
-        <link rel="stylesheet" href="/style.combined.min.css">
+        <link rel="stylesheet" href="/combined.min.css">
         <noscript><link rel="stylesheet" href="/noscript.css"></noscript>
     </head>
     <body>
